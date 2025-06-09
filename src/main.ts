38acf5e429d69226fd444c053438f4d31a7cd39b
rraaffly <muh.raflyrafsanjani@gmail.com>
2025-06-10 06:28:20 +0700

    chore: register modules

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono";
 
-import product from "$/handler/product";
-import order from "$/handler/order";
+import { Product } from "$/module/product";
+import { Order } from "$/module/order";
+import { Payment } from "$/module/payment";
+import { Photobox } from "$/module/photobox";
 
 const app = new Hono();
 
-app.route("/api/products", product);
-app.route("/api/orders", order);
+app.route("/api/products", Product);
+app.route("/api/orders", Order);
+app.route("/api/payments", Payment);
+app.route("/api/photobox", Photobox);
 
 export default app;
